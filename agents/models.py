6d289a2d2eb41f6668d800fe957b2fe36df051fc
Jermol Jupiter <jermol.jupiter@gmail.com>
2024-02-05 15:13:27 -0500

    Create Agent modle

---
+++ agents/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Agent(models.Model):
+    agent_type_choices = (
+        ("assistant", "assistant", ),
+        ("user_proxy", "user_proxy", ),
+    )
+    name = models.CharField(max_length=255, default="")
+    agent_type = models.CharField(max_length=255, default="assistant")
+
+    def __str__(self):
+        return self.name
+
+
+
