fd81089a11f8ae6a3897debc6f7680c57d98fd0e
Jermol Jupiter <jermol.jupiter@gmail.com>
2024-02-12 12:41:19 -0500

    Create Twitter Account model

--- twitter/models.py
+++ twitter/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Account(models.Model):
+    name = models.CharField(max_length=255, default="")
+    username = models.CharField(unique=True, max_length=255, default="")
+    user_id = models.BigIntegerField(default=0)
+    website = models.URLField(max_length=255)
+    linkedin = models.URLField(max_length=255)
+    medium = models.URLField(max_length=255)
+
+
