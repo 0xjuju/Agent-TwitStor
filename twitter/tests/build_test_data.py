a17b52a83b3b8d3cf53b88a91f140f26e2dba557
Jermol Jupiter <jermol.jupiter@gmail.com>
2024-02-13 16:30:08 -0500

    build test Accounts for Twitter Model

--- twitter/tests/build_test_data.py
+++ twitter/tests/build_test_data.py
@@ -5,5 +5,23 @@ class Build:
 
     @staticmethod
     def build_accounts():
-        pass
+
+        accounts = [
+            Account(
+                username="bloktopia",
+                website="Bloktopia.com",
+                linkedin="https://www.linkedin.com/company/bloktopia/?originalSubdomain=uk",
+                medium="https://medium.com/@bloktopia",
+                follower_count=328_000,
+            ),
+
+            Account(
+                username="AetherGamesInc",
+                website="https://aethergames.io/",
+                medium="https://medium.com/@AetherGames",
+                follower_count=105_000,
+            ),
+        ]
+
+        Account.objects.bulk_create(accounts)
 
