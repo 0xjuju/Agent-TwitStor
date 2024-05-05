e96392cd48f0b19a0e6feaba67a9f7e6e243590b
0xjuju <jermol.jupiter@gmail.com>
2024-05-05 12:42:31 -0400

    Move default prompts to isolated file

---
+++ agents/default_story_prompts.py
@@ -0,0 +1,18 @@
+
+
+default_prompts = {
+    "story_training_prompt": (
+        "System: You are a creative writing agent tasked with crafting a novel based on user "
+        "inputs. Whenever details are missing, intuitively fill them in to ensure the narrative "
+        "remains coherent. Emulate the style of the reference material without directly copying "
+        "any specific names, places, or identifiable details from the sources you learn from."
+    ),
+
+    "story_training_completion": (
+            "In your narratives, carefully initialize the tone, "
+            "setting, and characters to capture the reader's "
+            "interest. Ensure smooth transitions between chapters"
+            " and effectively build suspense from the beginning "
+            "and throughout the story."
+        )
+}
--- agents/prompts.py
+++ agents/prompts.py
@@ -1,26 +1,9 @@
-
+from agents.default_story_prompts import default_prompts
 
 """
 
 @use_default_prompt decorator is used to check if prompt=None. If it does, default_prompts are used.
 """
-
-default_prompts = {
-    "story_training_prompt": (
-        "System: You are a creative writing agent tasked with crafting a novel based on user "
-        "inputs. Whenever details are missing, intuitively fill them in to ensure the narrative "
-        "remains coherent. Emulate the style of the reference material without directly copying "
-        "any specific names, places, or identifiable details from the sources you learn from."
-    ),
-
-    "story_training_completion": (
-            "In your narratives, carefully initialize the tone, "
-            "setting, and characters to capture the reader's "
-            "interest. Ensure smooth transitions between chapters"
-            " and effectively build suspense from the beginning "
-            "and throughout the story."
-        )
-}
 
 
 def use_default_prompt(func):
